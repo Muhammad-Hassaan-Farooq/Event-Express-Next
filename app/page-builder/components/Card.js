98ad4574fc9444963a1d63d7b951ded44aa92519
Muhammad-Hassaan-Farooq <101121986+Muhammad-Hassaan-Farooq@users.noreply.github.com>
2024-05-27 18:33:10 +0500

    pagebuilder

--- app/page-builder/components/Card.js
+++ app/page-builder/components/Card.js
@@ -2,6 +2,7 @@ import { Text } from "./Text";
 import { Button } from "./Button";
 import { Container } from "./Container";
 import { useNode, Element } from "@craftjs/core";
+import { Container } from "./Container";
 
 export const Card = ({ background, padding = 20 }) => {
   return (
--- app/profile/page.js
+++ app/profile/page.js
@@ -1,27 +1,37 @@
 import { cookies } from "next/headers";
-import { jwtDecode } from 'jwt-decode';
-import axios from 'axios';
+import { jwtDecode } from "jwt-decode";
+import axios from "axios";
 import ProfilePage from "../components/ProfilePage";
 
 const MyProfile = async () => {
-    const token = cookies().get("token");
-    const decodedToken = jwtDecode(token.value);
-    const response = await axios.post("http://localhost:3000/profile/viewProfile", { 
-        id: decodedToken.id 
-    }, {
-        headers: {
-        Authorization: `Bearer ${token.value}`
-    }});
-
-    const handleDelete = async () => {
-        //Have to write details for delete button
+  const token = cookies().get("token");
+  const decodedToken = jwtDecode(token.value);
+  const response = await axios.post(
+    "http://localhost:3000/profile/viewProfile",
+    {
+      id: decodedToken.id,
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${token.value}`,
+      },
     }
-    return (
-        <div>
-            <ProfilePage firstName={response.data.data.firstName} lastName={response.data.data.lastName} 
-             email={response.data.data.email} role= {response.data.data.role} status={response.data.data.status} handleDelete={handleDelete}/>
-        </div>
-    )
-}
+  );
+
+  const handleDelete = async () => {
+    //Have to write details for delete button
+  };
+  return (
+    <div>
+      <ProfilePage
+        firstName={response.data.data.firstName}
+        lastName={response.data.data.lastName}
+        email={response.data.data.email}
+        role={response.data.data.role}
+        status={response.data.data.status}
+      />
+    </div>
+  );
+};
 
 export default MyProfile;
