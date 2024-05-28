a3eb4cbbc1de457c61262a63cdd7d08ed81f5ec9
Muhammad-Hassaan-Farooq <101121986+Muhammad-Hassaan-Farooq@users.noreply.github.com>
2024-05-28 21:29:55 +0500

    Cms updates

--- app/cms/components/CMSComponents/editableCards/EditHeroSection2.js
+++ app/cms/components/CMSComponents/editableCards/EditHeroSection2.js
@@ -1,14 +1,58 @@
-function EditableHeroSection2({ content }) {
+function EditableHeroSection2({
+  setComponentStates,
+  state,
+  section,
+  componentId,
+}) {
+  const { title, subtitle } = state;
+
+  const handleTitleChange = (e) => {
+    setComponentStates((prevStates) => ({
+      ...prevStates,
+      [section]: {
+        ...prevStates[section],
+        [componentId]: {
+          ...prevStates[section][componentId],
+          title: e.target.value,
+        },
+      },
+    }));
+  };
+
+  const handleSubtitleChange = (e) => {
+    setComponentStates((prevStates) => ({
+      ...prevStates,
+      [section]: {
+        ...prevStates[section],
+        [componentId]: {
+          ...prevStates[section][componentId],
+          subtitle: e.target.value,
+        },
+      },
+    }));
+  };
+
   return (
     <div className="form-group">
       <label for="title">Title</label>
-      <input type="text" className="form-control" id="title" />
+      <input
+        type="text"
+        className="form-control mb-3"
+        id="title"
+        value={title}
+        onChange={handleTitleChange}
+      />
       <label for="subtitle">Subtitle</label>
-      <input type="text" className="form-control" id="subtitle" />
+      <textarea
+        class="form-control mb-3"
+        aria-label="With textarea"
+        value={subtitle}
+        onChange={handleSubtitleChange}
+      ></textarea>
       <label for="formFile" class="form-label">
         Upload bg image
       </label>
-      <input class="form-control" type="file" id="formFile" />
+      <input class="form-control mb-3" type="file" id="formFile" />
     </div>
   );
 }
--- app/cms/page.js
+++ app/cms/page.js
@@ -8,14 +8,39 @@ import Slider from "./components/Slider";
 function Page() {
   const [currentlySelectedSection, setCurrentlySelectedSection] = useState("");
 
+  const [componentStates, setComponentStates] = useState({
+    Section1: {},
+    Section2: {},
+    Section3: {},
+    Section4: {},
+    Section5: {},
+  });
+
+  const [sections, setSections] = useState({
+    Section1: "",
+    Section2: "",
+    Section3: "",
+    Section4: "",
+    Section5: "",
+  });
+
   return (
     <>
       <PreviewButton />
       <MainPage
         setCurrentlySelectedSection={setCurrentlySelectedSection}
         currentlySelectedSection={currentlySelectedSection}
+        componentStates={componentStates}
+        setComponentStates={setComponentStates}
+        sections={sections}
+        setSections={setSections}
       />
-      <LeftMenu currentlySelectedSection={currentlySelectedSection} />
+      <LeftMenu
+        currentlySelectedSection={currentlySelectedSection}
+        sections={sections}
+        componentStates={componentStates}
+        setComponentStates={setComponentStates}
+      />
     </>
   );
 }
