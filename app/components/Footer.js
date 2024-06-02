c3489626b125f636d01bd8df2f7dbaf7f7242650
Muhammad-Hassaan-Farooq <101121986+Muhammad-Hassaan-Farooq@users.noreply.github.com>
2024-06-02 15:10:57 +0500

    .

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,72 +1,84 @@
-
+import "../styles/footer.css";
+import {
+  SlSocialFacebook,
+  SlSocialInstagram,
+  SlSocialGoogle,
+  SlSocialTwitter,
+  SlSocialGithub,
+  SlSocialLinkedin,
+} from "react-icons/sl";
 
 const Footer = () => {
-    return (
-        <div class="container my-5">
+  return (
+    <div className="">
+      <footer className="bg-light text-center text-white social-link">
+        <div className="container p-4 pb-0">
+          <section className="mb-4">
+            <a
+              className="btn btn-primary btn-floating m-1 social-link"
+              style={{ backgroundColor: "#3b5998" }}
+              href="#!"
+              role="button"
+            >
+              <SlSocialFacebook />
+            </a>
 
-            <footer class="bg-light text-center text-white">
+            <a
+              className="btn btn-primary btn-floating m-1 social-link"
+              style={{ backgroundColor: "#55acee" }}
+              href="#!"
+              role="button"
+            >
+              <SlSocialTwitter />
+            </a>
 
-                <div class="container p-4 pb-0">
+            <a
+              className="btn btn-primary btn-floating m-1 social-link"
+              style={{ backgroundColor: "#dd4b39" }}
+              href="#!"
+              role="button"
+            >
+              <SlSocialGoogle />
+            </a>
 
-                    <section class="mb-4">
+            <a
+              className="btn btn-primary btn-floating m-1 social-link"
+              style={{ backgroundColor: "#ac2bac" }}
+              href="#!"
+              role="button"
+            >
+              <SlSocialInstagram />
+            </a>
 
-                        <a
-                            class="btn btn-primary btn-floating m-1"
-                            style={{backgroundColor: "#3b5998"}}
-                            href="#!"
-                            role="button"
-                        ><i class="fab fa-facebook-f"></i></a>
+            <a
+              className="btn btn-primary btn-floating m-1 social-link"
+              style={{ backgroundColor: "#0082ca" }}
+              href="#!"
+              role="button"
+            >
+              <SlSocialLinkedin />
+            </a>
 
-                        <a
-                            class="btn btn-primary btn-floating m-1"
-                            style={{backgroundColor: "#55acee"}}
-                            href="#!"
-                            role="button"
-                        ><i class="fab fa-twitter"></i></a>
-
-
-                        <a
-                            class="btn btn-primary btn-floating m-1"
-                            style={{backgroundColor: "#dd4b39"}}
-                            href="#!"
-                            role="button"
-                        ><i class="fab fa-google"></i></a>
-
-
-                        <a
-                            class="btn btn-primary btn-floating m-1"
-                            style={{backgroundColor: "#ac2bac"}}
-                            href="#!"
-                            role="button"
-                        ><i class="fab fa-instagram"></i></a>
-
-
-                        <a
-                            class="btn btn-primary btn-floating m-1"
-                            style={{backgroundColor: "#0082ca"}}
-                            href="#!"
-                            role="button"
-                        ><i class="fab fa-linkedin-in"></i></a>
-
-                        <a
-                            class="btn btn-primary btn-floating m-1"
-                            style={{backgroundColor: "#333333"}}
-                            href="#!"
-                            role="button"
-                        ><i class="fab fa-github"></i></a>
-                    </section>
-
-                </div>
-
-                <div class="text-center p-3" style={{backgroundColor: "rgba(0, 0, 0, 0.2)"}}>
-                    © 2024 Copyright:
-                    <a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
-                </div>
-
-            </footer>
-
+            <a
+              className="btn btn-primary btn-floating m-1 social-link"
+              style={{ backgroundColor: "#333333" }}
+              href="#!"
+              role="button"
+            >
+              <SlSocialGithub />
+            </a>
+          </section>
         </div>
-    )
-}
+
+        <div
+          className="text-center p-3"
+          style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
+        >
+          © 2024 Copyright: Makra Inc
+        </div>
+      </footer>
+    </div>
+  );
+};
 
 export default Footer;
